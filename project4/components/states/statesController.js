5918d77f150308960a8696cfb30930be4b7e86bb
Austin Jones <austinjones@Austins-MacBook-Pro.local>
2019-02-05 21:54:05 -0800

    Finished logic for project 4.2. Need to go back and fix ng-models to add dots.

--- project4/components/states/statesController.js
+++ project4/components/states/statesController.js
@@ -7,6 +7,37 @@
 cs142App.controller('StatesController', ['$scope', function($scope) {
 
    // Replace this with the code for CS142 Project #4, Problem #2
-   console.log('window.cs142models.statesModel()', window.cs142models.statesModel());
+   // console.log('window.cs142models.statesModel()', JSON.stringify(window.cs142models.statesModel()));
 
+   if ($scope.main) {
+      $scope.main.title = 'CS142 Project #4 - States';
+   }
+
+   /*
+    * Stores original list of states.
+    */
+   $scope.states = window.cs142models.statesModel().sort();
+
+   /*
+    * Keeps track of which states are currently displayed. 
+    */
+   $scope.visibleStates = $scope.states;
+
+   /* 
+    * Keeps track of input field value.
+    */
+   $scope.inputString = '';
+
+   /* 
+    * Keeps track of the substring used to filter the states.
+    */
+   $scope.substring = '';
+
+   /* 
+    * Filters out states that don't contain the parameter as a substring.
+    */
+   $scope.filterStates = function(substring) {
+      $scope.visibleStates = substring ? $scope.states.filter(state => state.toLowerCase().includes(substring.toLowerCase())) : $scope.states; 
+      $scope.substring = substring;
+   }
 }]);

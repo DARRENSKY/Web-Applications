8dde4e6b8b9d79c86a43d93dedaacf0b0e2c6630
Austin Jones <austinjones@Austins-MacBook-Pro.local>
2019-02-06 14:06:22 -0800

    Switched to use ng-if instead of style.display to show different views.

--- project4/mainController.js
+++ project4/mainController.js
@@ -4,7 +4,7 @@
  * Create an angular module called 'cs142App' and assign it to a DOM property with the same name.
  * The [] argument specifies module is to be created and doesn't require any other module.
  */
-var cs142App = angular.module('cs142App', []);
+var cs142App = angular.module('cs142App', ['ngRoute']);
 
 /**
  * Create a controller named 'MainController'.  The array argument specifies the controller
@@ -14,9 +14,6 @@ var cs142App = angular.module('cs142App', []);
 cs142App.controller('MainController', ['$scope', function($scope) {
    // We defined an object called 'main' with a single property 'title' that is used
    // by the html view template to get the page's title in the browser tab.
-   // $scope.main = {};
-   // $scope.main.title = 'CS142 Project #4',
-
    $scope.main = {
       title: 'CS142 Project #4',
       hiddenView: 'States',
@@ -26,10 +23,7 @@ cs142App.controller('MainController', ['$scope', function($scope) {
     * Switches between the example and states views.
     */
    $scope.switchViews = function() {
-      let exampleView = document.getElementById('cs142-p4-example-view');
-      let statesView = document.getElementById('cs142-p4-states-view');
-      exampleView.style.display = $scope.main.hiddenView == 'States' ? 'none' : 'block';
-      statesView.style.display = $scope.main.hiddenView == 'States' ? 'block' : 'none';
       $scope.main.hiddenView = $scope.main.hiddenView == 'States' ? 'Example' : 'States';
+      $scope.main.title = $scope.main.hiddenView == 'States' ? 'CS142 Project #4 - Example' : 'CS142 Project #4 - States';
    };
 }]);
